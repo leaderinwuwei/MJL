9580aae975b3f6ce69465ddf7f15e6915b7fa45e
王锋 <captainwang1996@gmail.com>
2024-06-04 11:01:36 +0800

    reflect demo

---
+++ java/src/main/java/reflect/GenericType.java
@@ -0,0 +1,28 @@
+package reflect;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * @author CaptainWang
+ * @since 2024/6/3
+ */
+public class GenericType<T> {
+    private T data;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public static void main(String[] args) {
+        GenericType<String> genericType = new GenericType<String>() {};
+        Type superclass = genericType.getClass().getGenericSuperclass();
+        //getActualTypeArguments 返回确切的泛型参数, 如Map<String, Integer>返回[String, Integer]
+        Type type = ((ParameterizedType) superclass).getActualTypeArguments()[0];
+        System.out.println(type);//class java.lang.String
+    }
+}
