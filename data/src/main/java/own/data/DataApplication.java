70dd683d6ec529c6a83e7b856a35e336a0be4766
王锋 <captainwang1996@gmail.com>
2024-12-25 11:49:15 +0800

    format

--- data/src/main/java/own/data/DataApplication.java
+++ data/src/main/java/own/data/DataApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DataApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataApplication.class, args);
+    }
 }
---
+++ java/src/main/java/own/jdk21/BaseGrammar.java
@@ -0,0 +1,19 @@
+package own.jdk21;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author CaptainWang
+ * @since 2024/12/25
+ */
+public class BaseGrammar {
+
+    public static void main(String[] args) {
+
+        Queue queue = new LinkedList();
+        if (queue instanceof LinkedList linkedList) {
+            System.out.println(linkedList.size());
+        }
+    }
+}
